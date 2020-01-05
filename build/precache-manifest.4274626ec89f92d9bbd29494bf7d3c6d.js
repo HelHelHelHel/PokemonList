4b734600bfd4656e10d90e31cabb69642d63e583
Hel <helenahrubesova80@gmail.com>
2020-01-05 17:54:41 +0100

    first commit

---
+++ build/precache-manifest.4274626ec89f92d9bbd29494bf7d3c6d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86c2c10fe9d0e882a2ebb3b9a10ac65a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a3436ab3589284509cd6",
+    "url": "/static/js/2.415a9f95.chunk.js"
+  },
+  {
+    "revision": "2ddb796e2cfea5f03cbea984689c4c9d",
+    "url": "/static/js/2.415a9f95.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2aec8e13330d89f92e83",
+    "url": "/static/js/main.3f90aebe.chunk.js"
+  },
+  {
+    "revision": "f3f5290baee2c8ad6d1a",
+    "url": "/static/js/runtime-main.fec439dd.js"
+  }
+]);
